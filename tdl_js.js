42e420a33340c5e74d8e8b4360fae21baab07a59
Sergey Arenzon <sergey@sergey.lan>
2019-12-05 21:10:36 +0200

    done

---
+++ tdl_js.js
@@ -0,0 +1,46 @@
+
+
+
+
+$("ul").on("click","span",function (){  
+    $(this).parent().fadeOut(500,function () {
+        $(this).remove();
+    });
+})
+
+
+$("ul").on("click","li",function (){
+    $(this).toggleClass("lineThrough");    
+})
+
+
+$("#plusBtn").on("click",function(){
+   if($("#searchBox").css("display")==='block'){
+    $("#searchBox").fadeOut(500,function(){
+        $("#searchBox").css("display","none");
+    })
+   }
+   else{
+
+        $("#searchBox").fadeIn(500,function(){
+            $("#searchBox").css("display","block");
+        })
+   
+    }
+    
+})
+
+
+$("#searchBox").on("keypress",function(k){ 
+    if(k.which == 13){
+        var newTodo = $("#searchBox").val();
+        var newTodo = "<li><span><i class='far fa-trash-alt'></i></span>"+newTodo+"</li>"
+        if($("#searchBox").val() != ""){
+            $(newTodo).hide().fadeIn(500).appendTo("#toDos")
+        
+
+
+            $(this).val("");
+        }
+    }
+})
